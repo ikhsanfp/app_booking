12cbd27523335f1fd6a11596c39c7fbeb8ad99dc
Thesallonika KS <77711160+tessa138@users.noreply.github.com>
2024-04-26 11:35:07 +0700

    Update1 laporan.blade.php

--- resources/views/dashboard/admin/laporan.blade.php
+++ resources/views/dashboard/admin/laporan.blade.php
@@ -4,8 +4,8 @@
     <div>
         <h3 class="font-bold ml-12 text-left mt-16 mb-5">Pilih Tanggal Reservasi Untuk Melihat Laporan</h3>
     </div>
-    <div class="ml-12 mt-3 mb-4">
-        <input  class="p-2 border bg-gray-400 border-zinc-400 rounded-sm w-48" type="date" id="date" name="date" /><button class="font-bold text-white bg-blue-400 rounded px-5 py-2 ml-2 hover:bg-blue-800" type="submit">Lihat Laporan</button>
+   <div class="ml-12 mt-3 mb-4">
+        <input  class="p-2 border bg-gray-300 border-zinc-400 rounded-sm w-48" type="date" id="date" name="date" />
+        <a  href="{{ route('cetak.pesan') }}" target="_blank" class="font-bold text-white bg-blue-400 rounded px-5 py-2 ml-2 hover:bg-blue-800">Lihat Laporan</a>
     </div>
-
 @endsection
